ea7f5462e31341616e1440d14aeeb3ca6f248afb
Quan Wei <quan.wei@nifs.ac.jp>
2025-10-31 17:45:13 +0900

    make code(for)

--- code/sample.cpp
+++ code/sample.cpp
@@ -17,17 +17,20 @@ struct BCCData
 {
     double x1, y1, z1, xv1, yv1, zv1, xdv1, ydv1, zdv1;
 };
+std::vector<BCCData> BCC;
 //FCCData
 struct FCCData
 {
     double x2, y2, z2, xv2, yv2, zv2, xdv2, ydv2, zdv2;
 };
+std::vector<FCCData> FCC
 //DiamondData
 struct DiamondData
 {
     double x3, y3, z3, xv3, yv3, zv3, xdv3, ydv3, zdv3;
 };
-// read input data（now i don't konw energy 'E'）
+std::vector<FCCData> D
+// read input data(now i don't konw energy 'E')
 void read(BasicData &data)
 {
     cout << "please input energy 'E', cubic side length 'a0', box size 'Box_L': " 
@@ -51,7 +54,27 @@ bcc
 void bcc(FirstMolecularData &molecularData, BasicData &data,BCCData &Data)
 {
     int n = (data.Box_L/molecularData.x0)*(data.Box_L/molecularData.y0)*(data.Box_L/molecularData.z0);
+    for (int ix = 0;ix < nx; ++ix)
+    for(int iy = 0;iy < nx; ++iy)
+    for(int iz = 0;iz < nx; ++iz){
+        {
+            BCC a;
+            a.x = molecularData.x0 + data.a0 * (ix + 0.0);
+        a.y = molecularData.y0 + data.a0 * (iy + 0.0);
+        a.z = molecularData.z0 + data.a0 * (iz + 0.0);
 
+        a.vx = vel_dist(global_eng);
+        a.vy = vel_dist(global_eng);
+        a.vz = vel_dist(global_eng);
+
+        a.ax = 0.0;
+        a.ay = 0.0;
+        a.az = 0.0;
+        }
+        
+        
+        
+    }
 }
 
 // fcc
