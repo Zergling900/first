63744519e013ca63e2c3357e51b730eb5b41d171
Quan Wei <zergling903@gmail.com>
2025-11-09 21:40:00 +0900

    make v1.0

--- test2/basicdata.cpp
+++ test2/basicdata.cpp
@@ -18,9 +18,9 @@ Matrix3d CellVector(const BasicData &data)
     const double b = data.Cell_Lb;
     const double c = data.Cell_Lc;
 
-    // 
+    //
     const double alpha = data.cell_angle_alpha * M_PI / 180.0;
-    const double beta  = data.cell_angle_beta  * M_PI / 180.0;
+    const double beta = data.cell_angle_beta * M_PI / 180.0;
     const double gamma = data.cell_angle_gamma * M_PI / 180.0;
 
     const double ca = std::cos(alpha);
@@ -31,25 +31,21 @@ Matrix3d CellVector(const BasicData &data)
     Matrix3d Cell; // 3*3
 
     // 1 list a = a*(1, 0, 0)
-    Cell(0,0) = data.Cell_La;
-    Cell(1,0) = 0.0;
-    Cell(2,0) = 0.0;
+    Cell(0, 0) = data.Cell_La;
+    Cell(1, 0) = 0.0;
+    Cell(2, 0) = 0.0;
 
     // 2 list b = b*(cos(γ), sin(γ), 0 )
-    Cell(0,1) = data.Cell_Lb * cg;
-    Cell(1,1) = data.Cell_Lb * sg;
-    Cell(2,1) = 0.0;
+    Cell(0, 1) = data.Cell_Lb * cg;
+    Cell(1, 1) = data.Cell_Lb * sg;
+    Cell(2, 1) = 0.0;
 
     // 3 list c = c* (cos(β), (cos(α) - cos(β)*cos(γ)) / sin(γ),
     //                1 + 2*cos(α)*cos(β)*cos(γ)- cos(α)*cos(α)
     //                - cos(β)*cos(β)- cos(γ)*cos(γ)) / sin(γ))
-    Cell(0,2) = data.Cell_Lc * cb;
-    Cell(1,2) = data.Cell_Lc * (ca - cb * cg) / sg;
-    Cell(2,2) = data.Cell_Lc * std::sqrt
-    (
-        1.0 + 2.0 * ca * cb * cg
-        - ca * ca - cb * cb - cg * cg
-    ) / sg;
+    Cell(0, 2) = data.Cell_Lc * cb;
+    Cell(1, 2) = data.Cell_Lc * (ca - cb * cg) / sg;
+    Cell(2, 2) = data.Cell_Lc * std::sqrt(1.0 + 2.0 * ca * cb * cg - ca * ca - cb * cb - cg * cg) / sg;
 
     return Cell;
 }
@@ -59,24 +55,22 @@ Vector3d Step(const Matrix3d &Cell)
     return Vector3d(
         Cell.col(0).norm(),
         Cell.col(1).norm(),
-        Cell.col(2).norm()
-    );
+        Cell.col(2).norm());
 }
-
 
 // random (first molecular)
-std::mt19937 global_eng(std::random_device{}());
-Vector3d random(const BasicData &data, const Matrix3d &Cell)
-{
-double f = data.Cell_La;
+// std::mt19937 global_eng(std::random_device{}());
+// Vector3d random(const BasicData &data, const Matrix3d &Cell)
+// {
+//     double f = data.Cell_La;
 
-if (f < data.Cell_Lb)
-    f = data.Cell_Lb;
+//     if (f < data.Cell_Lb)
+//         f = data.Cell_Lb;
 
-if (f < data.Cell_Lc)
-    f = data.Cell_Lc;
-    std::uniform_real_distribution<> distr(0, 0.0 * f); 
-    Vector3d duvw(distr(global_eng), distr(global_eng), distr(global_eng));
-    return Cell * duvw;
-}
+//     if (f < data.Cell_Lc)
+//         f = data.Cell_Lc;
+//     std::uniform_real_distribution<> distr(0, 0.0 * f);
+//     Vector3d duvw(distr(global_eng), distr(global_eng), distr(global_eng));
+//     return Cell * duvw;
+// }
 //
--- test2/read.cpp
+++ test2/read.cpp
@@ -7,7 +7,7 @@
 #include <eigen3/Eigen/Dense>
 
 #include "2.h"
-#include "parameter.p1"
+// #include "parameter.p1"
 //#include "parameter.p2"
 
 using namespace Eigen;
@@ -16,8 +16,9 @@ using namespace std;
 
 #include <iostream>
 
-void read(BasicData &data)
+void read1(BasicData &data, vector<Atom> &atoms)
 {
+    data.data_file_name = "error.md3";
     ifstream fin("parameter.p1");
     if (!fin) {
         cerr << "con't open the file: " << "parameter.p1" << endl;
@@ -34,22 +35,82 @@ void read(BasicData &data)
             continue; 
 
         stringstream ss(line);
+        ss >> key;
+
+        if (key == "data_file_name") //string(key) otput file name
+        {
+            ss >> data.data_file_name;
+        }
+        else
+        {
+            ss >> value;
+            
+            if (key == "time") data.T = value;
+//            else if (key == "n") data.n =value;
+            else if (key == "energy") data.E = value;
+            else if (key == "Cell_La") data.Cell_La = value;
+            else if (key == "Cell_Lb") data.Cell_Lb = value;
+            else if (key == "Cell_Lc") data.Cell_Lc = value;
+            else if (key == "cell_angle_alpha") data.cell_angle_alpha = value;
+            else if (key == "cell_angle_beta") data.cell_angle_beta = value;
+            else if (key == "cell_angle_gamma") data.cell_angle_gamma = value;
+            else if (key == "Box_Ln") data.Box_Ln = value;
+            else if (key == "Box_Lx") data.Box_Lx = value;
+            else if (key == "Box_Ly") data.Box_Ly = value;
+            else if (key == "Box_Lz") data.Box_Lz = value;
+            else if (key == "coordinates") 
+            {
+                while (getline(fin, line)) 
+                {
+                    if (line.empty() || 
+                        line[0] == '#' || 
+                        (line.size() > 1 && line[0] == '/' && line[1] == '/'))
+                        continue; 
+                    else
+                    {
+                        Atom atom;
+                        stringstream ss(line);
+                        ss >> atom.type >> atom.label >> atom.symmetry_multiplicity 
+                        >> atom.x0 >> atom.y0 >> atom.z0 >> atom.occupancy;
+                        atoms.push_back(atom);
+                    }
+                }
+            }
+        }
+    }
+
+    fin.close();
+}
+
+void read2(Data &data)
+{
+    ifstream fin("parameter.p2");
+    if (!fin) {
+        cerr << "con't open the file: " << "parameter.p2" << endl;
+        return;
+    }
+
+    string line, key;
+    double value;
+
+    while (getline(fin, line)) {
+        if (line.empty() || 
+            line[0] == '#' || 
+            (line.size() > 1 && line[0] == '/' && line[1] == '/'))
+            continue; 
+
+        stringstream ss(line);
         ss >> key >> value;
 
-        if (key == "energy") data.E = value;
-        else if (key == "time") data.T = value;
-
-        else if (key == "Cell_La") data.Cell_La = value;
-        else if (key == "Cell_Lb") data.Cell_Lb = value;
-        else if (key == "Cell_Lc") data.Cell_Lc = value;
-        else if (key == "cell_angle_alpha") data.cell_angle_alpha = value;
-        else if (key == "cell_angle_beta") data.cell_angle_beta = value;
-        else if (key == "cell_angle_gamma") data.cell_angle_gamma = value;
-        else if (key == "Box_Ln") data.Box_Ln = value;
-        else if (key == "Box_Lx") data.Box_Lx = value;
-        else if (key == "Box_Ly") data.Box_Ly = value;
-        else if (key == "Box_Lz") data.Box_Lz = value;
-        else if (key == "coordinates") 
+        if (key == "x0") data.x = value;
+        else if (key == "y0") data.y = value;
+        else if (key == "z0") data.z = value;
+        else if (key == "xv0") data.vx = value;
+        else if (key == "yv0") data.vy = value;
+        else if (key == "zv0") data.vz = value;
+        else if (key == "dvx0") data.dvx = value;
+        else if (key == "dvy") data.dvy = value;
+        else if (key == "dvz") data.dvz = value;
     }
 
     fin.close();
