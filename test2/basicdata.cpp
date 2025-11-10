ed20eeb0758b8ff5fe728412bf442f4dbebb4b5b
Quan Wei <quan.wei@nifs.ac.jp>
2025-11-10 12:18:42 +0900

    change some parameter name

--- test2/basicdata.cpp
+++ test2/basicdata.cpp
@@ -14,14 +14,14 @@ using namespace std;
 // ----------------------------
 Matrix3d CellVector(const BasicData &data)
 {
-    const double a = data.Cell_La;
-    const double b = data.Cell_Lb;
-    const double c = data.Cell_Lc;
+    const double a = data._cell_length_a;
+    const double b = data._cell_length_b;
+    const double c = data._cell_length_c;
 
     //
-    const double alpha = data.cell_angle_alpha * M_PI / 180.0;
-    const double beta = data.cell_angle_beta * M_PI / 180.0;
-    const double gamma = data.cell_angle_gamma * M_PI / 180.0;
+    const double alpha = data._cell_angle_alpha * M_PI / 180.0;
+    const double beta = data._cell_angle_beta * M_PI / 180.0;
+    const double gamma = data._cell_angle_gamma * M_PI / 180.0;
 
     const double ca = std::cos(alpha);
     const double cb = std::cos(beta);
@@ -31,21 +31,21 @@ Matrix3d CellVector(const BasicData &data)
     Matrix3d Cell; // 3*3
 
     // 1 list a = a*(1, 0, 0)
-    Cell(0, 0) = data.Cell_La;
+    Cell(0, 0) = data._cell_length_a;
     Cell(1, 0) = 0.0;
     Cell(2, 0) = 0.0;
 
     // 2 list b = b*(cos(γ), sin(γ), 0 )
-    Cell(0, 1) = data.Cell_Lb * cg;
-    Cell(1, 1) = data.Cell_Lb * sg;
+    Cell(0, 1) = data._cell_length_b * cg;
+    Cell(1, 1) = data._cell_length_b * sg;
     Cell(2, 1) = 0.0;
 
     // 3 list c = c* (cos(β), (cos(α) - cos(β)*cos(γ)) / sin(γ),
     //                1 + 2*cos(α)*cos(β)*cos(γ)- cos(α)*cos(α)
     //                - cos(β)*cos(β)- cos(γ)*cos(γ)) / sin(γ))
-    Cell(0, 2) = data.Cell_Lc * cb;
-    Cell(1, 2) = data.Cell_Lc * (ca - cb * cg) / sg;
-    Cell(2, 2) = data.Cell_Lc * std::sqrt(1.0 + 2.0 * ca * cb * cg - ca * ca - cb * cb - cg * cg) / sg;
+    Cell(0, 2) = data._cell_length_c * cb;
+    Cell(1, 2) = data._cell_length_c * (ca - cb * cg) / sg;
+    Cell(2, 2) = data._cell_length_c * std::sqrt(1.0 + 2.0 * ca * cb * cg - ca * ca - cb * cb - cg * cg) / sg;
 
     return Cell;
 }
@@ -62,13 +62,13 @@ Vector3d Step(const Matrix3d &Cell)
 // std::mt19937 global_eng(std::random_device{}());
 // Vector3d random(const BasicData &data, const Matrix3d &Cell)
 // {
-//     double f = data.Cell_La;
+//     double f = data._cell_length_a;
 
-//     if (f < data.Cell_Lb)
-//         f = data.Cell_Lb;
+//     if (f < data._cell_length_b)
+//         f = data._cell_length_b;
 
-//     if (f < data.Cell_Lc)
-//         f = data.Cell_Lc;
+//     if (f < data._cell_length_c)
+//         f = data._cell_length_c;
 //     std::uniform_real_distribution<> distr(0, 0.0 * f);
 //     Vector3d duvw(distr(global_eng), distr(global_eng), distr(global_eng));
 //     return Cell * duvw;
