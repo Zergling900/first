ad206eedebeddfb912b3fa57fd398f88809e61e6
Quan Wei <zergling903@gmail.com>
2025-11-12 20:03:20 +0900

    make new file--3

---
+++ test3/3.h
@@ -0,0 +1,28 @@
+#pragma once
+
+#include <iostream>
+#include <random>
+#include <cmath>
+#include <fstream>
+#include <vector>
+#include <iomanip>
+#include <eigen3/Eigen/Dense>
+
+
+using namespace Eigen;
+using namespace std;
+
+// ----------------------------
+
+struct firstdata
+{
+    int n;                              // number of atoms
+    double T;                           // unknown
+    double E;                           // energy
+    double Box_Ln, Box_Lx,Box_Ly,Box_Lz;// box size
+    string name;
+    double x, y, z;                     //position
+    double vx, vy, vz;                  //velocity
+    double dvx, dvy, dvz;               //acceleration
+};
+
