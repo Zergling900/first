b63468ab13df7afad4a4f2e61249676b732e6113
Quan Wei <quan.wei@nifs.ac.jp>
2025-11-14 15:54:26 +0900

    read0&1

--- test3/read.cpp
+++ test3/read.cpp
@@ -1,11 +1,11 @@
 #include <iostream>
-//#include <random>
+// #include <random>
 #include <sstream>
 #include <string>
-//#include <cmath>
-//#include <fstream>
+// #include <cmath>
+// #include <fstream>
 #include <vector>
-//#include <iomanip>
+// #include <iomanip>
 
 #include "3.h"
 
@@ -15,7 +15,8 @@ using namespace std;
 void read0(const FileName &filename, Data &data, std::vector<Data> &atoms)
 {
     ifstream fin(filename.BasicData_filename);
-    if (!fin) {
+    if (!fin)
+    {
         std::cerr << "can't open file: " << filename.BasicData_filename << std::endl;
         return;
     }
@@ -23,38 +24,60 @@ void read0(const FileName &filename, Data &data, std::vector<Data> &atoms)
     // 1. n
     fin >> data.n;
     string dummy;
-    getline(fin, dummy);  //skip line
+    getline(fin, dummy); // skip line
 
     // 2. time & Energy
     string tmp;
-    fin >> tmp;        // "time="
-    fin >> data.T;   // T
-    fin >> tmp;        // "(fs)"
-    fin >> tmp;        // "Energy="
-    fin >> data.E;   // E
-    getline(fin, dummy);  //skip line
+    fin >> tmp;          // "time="
+    fin >> data.T;       // T
+    fin >> tmp;          // "(fs)"
+    fin >> tmp;          // "Energy="
+    fin >> data.E;       // E
+    getline(fin, dummy); // skip line
 
     // 3. BOX
     fin >> tmp; // "BOX"
-    fin >> data.Box.a00 >> data.Box.a10 >> data.Box.a20
-        >> data.Box.a01 >> data.Box.a11 >> data.Box.a21
-        >> data.Box.a02 >> data.Box.a12 >> data.Box.a22;
+    fin >> data.Box.a00 >> data.Box.a10 >> data.Box.a20 >> data.Box.a01 >> data.Box.a11 >> data.Box.a21 >> data.Box.a02 >> data.Box.a12 >> data.Box.a22;
 
     // 4. atoms
     atoms.clear();
     atoms.reserve(data.n);
 
-    for (int i = 0; i < data.n - 1; ++i) {
+    for (int i = 0; i < data.n - 1; ++i)
+    {
         Data d;
-        fin >> d.name
-            >> d.x  >> d.y  >> d.z
-            >> d.vx >> d.vy >> d.vz
-            >> d.dvx >> d.dvy >> d.dvz;
+        fin >> d.name >> d.x >> d.y >> d.z >> d.vx >> d.vy >> d.vz >> d.dvx >> d.dvy >> d.dvz;
         atoms.push_back(d);
     }
 }
 
-void read1()
+void read1(parameter1 &p1)
 {
-    
+    ifstream fin("parameter.p1");
+    if (!fin)
+    {
+        std::cerr << "can't open file: " << "parameter.p1" << std::endl;
+        return;
+    }
+
+    string key;
+    double value;
+    int i;
+    while (fin >> key >> value)
+    {
+        if (key == "dt")
+        {
+            p1.dt = value;
+        }
+        else if (key == "steps")
+        {
+            p1.steps = static_cast<int>(value);
+        }
+        else if (key == "steps_space")
+        {
+            p1.steps_space = static_cast<int>(value);
+        }
+    }
+
+    fin.close();
 }
