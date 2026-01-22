6803ad19ecc73f95c1339cb3204b227bc1d48d2e
Quan Wei <zergling903@gmail.com>
2026-01-23 02:02:15 +0900

    home 6

--- LCL/lcl.cpp
+++ LCL/lcl.cpp
@@ -8,12 +8,11 @@ struct Cell_List
 {
     double Wx,Wy,Wz;
     int Mx,My,Mz,cell_num;
-    std::vector<int> Atom;      //N
-    std::vector<int> Cell;
-    std::vector<int> num_in_cell;  //M number of particles in each cell
+    std::vector<int> Cell;//N
+    //std::vector<int> num_in_cell;  //M number of particles in each cell
     std::vector<int> cell_offset;  // The starting point of cell[i] in [atom_indices]
     std::vector<int> atom_indices; // Install the particle IDs in segments into cells(每个粒子在所在的格子中的位置（第几个）)
-    std::vector<int> atom_order;   // Local order of particle[i] within its cell
+    //std::vector<int> atom_order;   // Local order of particle[i] within its cell
 };
 void lcl0(Data&data,Cell_List&cl,const parameter1 &pr1, const parameter2 &pr2)
 {
@@ -27,7 +26,7 @@ void lcl0(Data&data,Cell_List&cl,const parameter1 &pr1, const parameter2 &pr2)
 
     cl.cell_num = cl.Mx * cl.My * cl.Mz;
 
-    cl.Atom.resize(data.n);
+    cl.Cell.resize(data.n);
     cl.cell_offset.resize(cl.cell_num + 1);
     cl.atom_indices.resize(data.n);
 }
@@ -37,7 +36,7 @@ void lcl1(Data&data,Cell_List&cl,const parameter1 &pr1, const parameter2 &pr2)
     double Wx = cl.Wx;
     double Wy = cl.Wy;
     double Wz = cl.Wz;
-    std::vector<int> num_cell;
+    std::vector<int> num_in_cell(cl.cell_num, 0);
 
     for(int i=0;i<data.n;i++)
     {
@@ -45,12 +44,28 @@ void lcl1(Data&data,Cell_List&cl,const parameter1 &pr1, const parameter2 &pr2)
         double my = floor(data.atoms[i].r.a10 / Wy);
         double mz = floor(data.atoms[i].r.a20 / Wz);
         cl.Cell[i] = mx + my * cl.Mx + mz * cl.Mx * cl.My;
-        num_cell[cl.Cell[i]] ++; 
+        num_in_cell[cl.Cell[i]] ++; 
     }
     
     cl.cell_offset[0] = 0;
     for(int i=0;i<cl.cell_num;i++)
     {
-
+        cl.cell_offset[i+1] = cl.cell_offset[i] + num_in_cell[i];
     }
+
+    std::vector<int> offset1(cl.cell_num);
+    for(int i=0;i<cl.cell_num;i++)
+    {
+        offset1[i] = cl.cell_offset[i];
+    }
+
+    for(int i=0;i<data.n;i++)
+    {
+        int c = cl.Cell[i];//读取第i个粒子所在的格子编号
+        int pos = offset1[c]++;//该格子中粒子的个数加1，并返回该粒子在该格子中的位置
+        cl.atom_indices[pos] = i;//将粒子i的编号存入该格子中
+    }
+
+
+
 }
